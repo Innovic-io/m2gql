091429b6de1b238abbb45ca591bfad7af0521ce9
vladimir <vladimir.kunarac@gmail.com>
2018-03-21 11:44:19 +0100

    removed mongo dependencie

--- src/data.fetch.js
+++ src/data.fetch.js
@@ -8,96 +8,96 @@ mongo.Collection.prototype.getData = function () {
             .toArray()
     }
 
-    return this.aggregate( [
-                // Stage 1
-                {
-                    $unwind: "$properties"
-                },
+    return this.aggregate([
+        // Stage 1
+        {
+            $unwind: "$properties"
+        },
 
-                // Stage 2
-                {
-                    $lookup: {
-                        "from": "parameters",
-                        "localField": "properties.definition",
-                        "foreignField": "_id",
-                        "as": "parameters"
-                    }
-                },
+        // Stage 2
+        {
+            $lookup: {
+                "from": "parameters",
+                "localField": "properties.definition",
+                "foreignField": "_id",
+                "as": "parameters"
+            }
+        },
 
-                // Stage 3
-                {
-                    $unwind: "$parameters"
-                },
+        // Stage 3
+        {
+            $unwind: "$parameters"
+        },
 
-                // Stage 4
-                {
-                    $project: {
-                        "_id": 1,
-                        "model": 1,
-                        "company": 1,
-                        "type": 1,
-                        "complex": 1,
-                        "entity": 1,
-                        "parameter": {
-                            "name": "$parameters.name",
-                            "value": "$properties.value.current"
+        // Stage 4
+        {
+            $project: {
+                "_id": 1,
+                "model": 1,
+                "company": 1,
+                "type": 1,
+                "complex": 1,
+                "entity": 1,
+                "parameter": {
+                    "name": "$parameters.name",
+                    "value": "$properties.value.current"
+                }
+            }
+        },
+
+        // Stage 5
+        {
+            $group: {
+                _id: "$_id",
+                rootComapny: {"$first": "$company"},
+                rootComplex: {"$first": "$complex"},
+                rootEntity: {"$first": "$entity"},
+                rootModel: {"$first": "$model"},
+                rootType: {"$first": "$type"},
+                parameters: {$push: "$parameter"}
+            }
+        },
+
+        // Stage 6
+        {
+            $project: {
+                _id: 1,
+                rootComapny: 1,
+                rootComplex: 1,
+                rootEntity: 1,
+                rootModel: 1,
+                rootType: 1,
+                replacingRoot: {
+                    $arrayToObject: {
+                        $map: {
+                            input: "$parameters",
+                            as: "pair",
+                            in: [ "$$pair.name", "$$pair.value" ]
                         }
                     }
-                },
+                }
+            }
+        },
 
-                // Stage 5
-                {
-                    $group: {
-                        _id: "$_id",
-                        rootComapny: {"$first": "$company"},
-                        rootComplex: {"$first": "$complex"},
-                        rootEntity: {"$first": "$entity"},
-                        rootModel: {"$first": "$model"},
-                        rootType: {"$first": "$type"},
-                        parameters: {$push: "$parameter"}
-                    }
-                },
+        // Stage 7
+        {
+            $addFields: {
+                "replacingRoot.company": "$rootComapny",
+                "replacingRoot.complex": "$rootComplex",
+                "replacingRoot.type": "$rootType",
+                "replacingRoot.model": "$rootModel",
+                "replacingRoot.entity": "$rootEntity",
+                "replacingRoot._id": "$_id"
+            }
+        },
 
-                // Stage 6
-                {
-                    $project: {
-                        _id: 1,
-                        rootComapny: 1,
-                        rootComplex: 1,
-                        rootEntity: 1,
-                        rootModel: 1,
-                        rootType: 1,
-                        replacingRoot: {
-                            $arrayToObject: {
-                                $map: {
-                                    input: "$parameters",
-                                    as: "pair",
-                                    in: [ "$$pair.name", "$$pair.value" ]
-                                }
-                            }
-                        }
-                    }
-                },
-
-                // Stage 7
-                {
-                    $addFields: {
-                        "replacingRoot.company": "$rootComapny",
-                        "replacingRoot.complex": "$rootComplex",
-                        "replacingRoot.type": "$rootType",
-                        "replacingRoot.model": "$rootModel",
-                        "replacingRoot.entity": "$rootEntity",
-                        "replacingRoot._id": "$_id"
-                    }
-                },
-
-                // Stage 8
-                {
-                    $replaceRoot: {
-                        newRoot: "$replacingRoot"
-                    }
-                },
-            ])
+        // Stage 8
+        {
+            $replaceRoot: {
+                newRoot: "$replacingRoot"
+            }
+        },
+    ])
         .toArray();
 };
 
