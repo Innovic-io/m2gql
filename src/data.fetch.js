8df6d7a84018a0a9b57ca29c7976d32ca8b94436
vladimir <vladimir.kunarac@gmail.com>
2018-03-26 16:30:52 +0200

    To be continued

--- src/data.fetch.js
+++ src/data.fetch.js
@@ -1,105 +1,147 @@
 const mongo = require('mongodb');
 
-mongo.Collection.prototype.getData = function () {
+mongo.Collection.prototype.getData = async function () {
+    switch (this.s.name) {
+        case 'ocean':
 
-    if (this.s.name !== 'objects') {
-        return this
-            .find({})
-            .toArray()
-    }
+            const result = await this.find({}).toArray();
 
-    return this.aggregate([
-        // Stage 1
-        {
-            $unwind: "$properties"
-        },
+            return result.map((element) => Object.assign({}, ...Object.keys(element)
+                    .filter((singleKey) => singleKey !== 'relations')
+                    .filter((singleKey) => singleKey !== 'properties')
+                    .filter((singleKey) => singleKey !== 'meta')
+                    .map((singleKey) => Object.assign({}, {[singleKey]: element[singleKey]},
+                        makeMeta(element.meta),
+                        makeProperties(element.properties))
+                    )));
 
-        // Stage 2
-        {
-            $lookup: {
-                "from": "parameters",
-                "localField": "properties.definition",
-                "foreignField": "_id",
-                "as": "parameters"
-            }
-        },
+        case 'objects':
+            return this.aggregate([
+            // Stage 1
+            {
+                $unwind: "$properties"
+            },
 
-        // Stage 3
-        {
-            $unwind: "$parameters"
-        },
-
-        // Stage 4
-        {
-            $project: {
-                "_id": 1,
-                "model": 1,
-                "company": 1,
-                "type": 1,
-                "complex": 1,
-                "entity": 1,
-                "parameter": {
-                    "name": "$parameters.name",
-                    "value": "$properties.value.current"
+            // Stage 2
+            {
+                $lookup: {
+                    "from": "parameters",
+                    "localField": "properties.definition",
+                    "foreignField": "_id",
+                    "as": "parameters"
                 }
-            }
-        },
+            },
 
-        // Stage 5
-        {
-            $group: {
-                _id: "$_id",
-                rootComapny: {"$first": "$company"},
-                rootComplex: {"$first": "$complex"},
-                rootEntity: {"$first": "$entity"},
-                rootModel: {"$first": "$model"},
-                rootType: {"$first": "$type"},
-                parameters: {$push: "$parameter"}
-            }
-        },
+            // Stage 3
+            {
+                $unwind: "$parameters"
+            },
 
-        // Stage 6
-        {
-            $project: {
-                _id: 1,
-                rootComapny: 1,
-                rootComplex: 1,
-                rootEntity: 1,
-                rootModel: 1,
-                rootType: 1,
-                replacingRoot: {
-                    $arrayToObject: {
-                        $map: {
-                            input: "$parameters",
-                            as: "pair",
-                            in: [ "$$pair.name", "$$pair.value" ]
+            // Stage 4
+            {
+                $project: {
+                    "_id": 1,
+                    "model": 1,
+                    "company": 1,
+                    "type": 1,
+                    "complex": 1,
+                    "entity": 1,
+                    "parameter": {
+                        "name": "$parameters.name",
+                        "value": "$properties.value.current"
+                    }
+                }
+            },
+
+            // Stage 5
+            {
+                $group: {
+                    _id: "$_id",
+                    rootComapny: {"$first": "$company"},
+                    rootComplex: {"$first": "$complex"},
+                    rootEntity: {"$first": "$entity"},
+                    rootModel: {"$first": "$model"},
+                    rootType: {"$first": "$type"},
+                    parameters: {$push: "$parameter"}
+                }
+            },
+
+            // Stage 6
+            {
+                $project: {
+                    _id: 1,
+                    rootComapny: 1,
+                    rootComplex: 1,
+                    rootEntity: 1,
+                    rootModel: 1,
+                    rootType: 1,
+                    replacingRoot: {
+                        $arrayToObject: {
+                            $map: {
+                                input: "$parameters",
+                                as: "pair",
+                                in: [ "$$pair.name", "$$pair.value" ]
+                            }
                         }
                     }
                 }
-            }
-        },
+            },
 
-        // Stage 7
-        {
-            $addFields: {
-                "replacingRoot.company": "$rootComapny",
-                "replacingRoot.complex": "$rootComplex",
-                "replacingRoot.type": "$rootType",
-                "replacingRoot.model": "$rootModel",
-                "replacingRoot.entity": "$rootEntity",
-                "replacingRoot._id": "$_id"
-            }
-        },
+            // Stage 7
+            {
+                $addFields: {
+                    "replacingRoot.company": "$rootComapny",
+                    "replacingRoot.complex": "$rootComplex",
+                    "replacingRoot.type": "$rootType",
+                    "replacingRoot.model": "$rootModel",
+                    "replacingRoot.entity": "$rootEntity",
+                    "replacingRoot._id": "$_id"
+                }
+            },
 
-        // Stage 8
-        {
-            $replaceRoot: {
-                newRoot: "$replacingRoot"
-            }
-        },
-    ])
-        .toArray();
+            // Stage 8
+            {
+                $replaceRoot: {
+                    newRoot: "$replacingRoot"
+                }
+            },
+        ])
+            .toArray();
+
+        default:
+            return this.find({}).toArray()
+    }
 };
+
+function makeMeta(meta) {
+
+    if(!meta) {
+        return {};
+    }
+
+    return Object.assign({}, ...Object
+        .keys(meta)
+        .map((singleKey) => Object.assign({}, {[singleKey]: meta[singleKey].value})));
+}
+
+function makeProperties(properties) {
+
+    if(properties.length === 0) {
+        return {};
+    }
+    const endingObject = [];
+    properties
+        .forEach((singleElem) =>
+            Object
+                .keys(singleElem)
+                .forEach((singleKey) =>
+                    endingObject
+                        .push({[singleElem[singleKey].definition.name]: singleElem[singleKey].value.value})
+        )
+    );
+
+    return Object.assign({}, ...endingObject);
+}
 
 async function getCollectionNames(database) {
     return await database
@@ -113,6 +155,7 @@ async function getAllCollectionsData(database) {
     const collections = await getCollectionNames(database);
     const output = [];
     for (const collection of collections) {
+
         output.push({
             [ collection ]: await database.collection(collection)
                 .getData()
