c7805cc8e1731bd7e99ac2789e5eab60b892b244
vladimir <vladimir.kunarac@gmail.com>
2018-04-05 11:10:09 +0200

    improvements

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ interface ICollection {
     [collectionName: string]: object[] | object
 }
 declare module 'mongodb-to-graphql2' {
-    export async function createFromDB(databaseURI: string, collectionName?: string, modelName?: string, companyName?: string): string
+    export function createFromDB(databaseURI: string, collectionName?: string, modelName?: string, companyName?: string): Promise<string>
 
     export function createGraphQL(collectionData: ICollection | ICollection[]): string;
 }
