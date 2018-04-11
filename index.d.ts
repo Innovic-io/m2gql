a6ba3b0af0af899397d7fb42c4688531d132eb94
Vlado Tešanović <vlado.tesanovic@gmail.com>
2018-04-11 09:00:15 +0200

    chainging name to m2gql

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 interface ICollection {
     [collectionName: string]: object[] | object
 }
-declare module 'm2qql' {
+declare module 'm2gql' {
     export function createFromDB(databaseURI: string, collectionName?: string, modelName?: string, companyName?: string): Promise<string>
     export function createGraphQL(collectionData: ICollection | ICollection[]): string;
 }
